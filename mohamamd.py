477aecb3c75351975e760337ce1986c5a061e23f
Mohammad Moraveji <ms.moraveji2007@gmail.com>
2022-10-21 11:20:18 +0330

    Update and rename mohamamds to mohamamd.py

---
+++ mohamamd.py
@@ -0,0 +1,17 @@
+#time in pyton:
+# We will use the time module
+import time
+ 
+# Create a function that will print the time
+def create_countdown_timer(time):
+    print(time,"......")
+ 
+time_in_sec=int(input("Please entert the time in seconds:"))
+ 
+for times in range(time_in_sec):
+    # call the function and pass the current time left
+    create_countdown_timer(time_in_sec-times)
+    # call the function in every 1 second.
+    time.sleep(1)
+     
+print("Finish")
